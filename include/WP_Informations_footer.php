d4d7ee79c658245bbf94ab4ba9f7c06bacf95e8f
vic4dev <victor.dlf@outlook.fr>
2016-11-21 15:34:30 +0100

    delete include

--- include/WP_Informations_footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Add footer -->
-<footer class="wp_footer">
-	<span id="footer-left">Si vous aimez utiliser WP Informations, veuillez nous évaluer sur <a target="_blank" href="https://wordpress.org/">WordPress.org</a>.</span>
-	<span id="footer-right">Version 1.0</span>
-</footer>
