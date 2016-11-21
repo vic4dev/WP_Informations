e0c02f4a6809d45a122bc5f942c7e0859be1ccf4
vic4dev <victor.dlf@outlook.fr>
2016-11-21 15:36:47 +0100

    update files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#Bouton").onclick = function() {
-    if (window.getComputedStyle(document.querySelector('#tonDiv')).display == 'none') {
-        document.querySelector("#tonDiv").style.display = "block";
-    } else {
-        document.querySelector("#tonDiv").style.display = "none";
-        $('#Bouton').addClass('rouge');    
-    }
-}
